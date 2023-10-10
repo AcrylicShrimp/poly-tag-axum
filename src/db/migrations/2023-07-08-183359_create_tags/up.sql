afb5e5a77225b011332847d68b24b9fc7caf314c
AcrylicShrimp <led789zxpp@naver.com>
2023-10-11 06:37:19 +0900

    feat: add indices.

--- src/db/migrations/2023-07-08-183359_create_tags/up.sql
+++ src/db/migrations/2023-07-08-183359_create_tags/up.sql
@@ -8,4 +8,12 @@ CREATE TABLE tags (
   value_boolean BOOLEAN NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE INDEX ON tags(template_uuid ASC);
+CREATE INDEX ON tags(file_uuid ASC);
+CREATE INDEX ON tags(value_string ASC);
+CREATE INDEX ON tags(value_integer ASC);
+CREATE INDEX ON tags(value_boolean ASC);
 CREATE INDEX ON tags(created_at ASC);
+CREATE INDEX ON tags(template_uuid ASC, value_string ASC);
+CREATE INDEX ON tags(template_uuid ASC, value_integer ASC);
+CREATE INDEX ON tags(template_uuid ASC, value_boolean ASC);
